845635761d4002dcd928c9fb0142e8d60c50fb7e
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-05-01 21:54:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Book collection parser
+This program parses a collection of science fiction books of a 
+certain genre from [Tululu online library](http://tululu.org/).
+The default genre set is [Science Fiction](http://tululu.org/l55/)
+with code `\l55\`. 
+
+
+### Getting started
+User can download books in `txt` 
+format and books covers images. 
+The program also generates a `json` format file with downloaded 
+books data such as:
+- book title
+- author
+- book `.txt` file path
+- book cover image path
+- genres
+- comments
+
+The collection on Tululu includes a 
+certain number of pages, which you can choose to be 
+the first (`start_page`) and the last (`end_page`) 
+pages to download the books from.
+
+The program is launched from the command line
+with __optional arguments__:
+- `start_page` — The first page to parse. 
+If start_page is not set as an
+argument by user, it will be set as page number 1.
+- `end_page` — The last page to parse. 
+If end_page is not set as an
+argument by user, it will be set as the last page of
+the collection.
+- `skip_txt` — Do not download book `.txt` files. 
+The program will download `book.txt` files to `books/` directory,
+ if the argument 
+is not set by user.
+- `skip_img` — Do not download book covers images. 
+The program will download images files to `images/` directory, 
+if the argument 
+is not set by user.
+- `dest_folder` — This argument lets user set the path for
+downloaded files.
+- `json_path` — This argument lets user set the path for 
+generated `books.json` file only.
+
+
+### How to use
+1. Download `parse_tululu_category.py` script.
+2. In the command line enter:
+    ```commandline
+    python parse_tululu_category.py optional_arguments
+    ```
+   
+##### Examples
+Download the books `.txt` files and covers images from page 5 to page 10 of the collection to 
+and create `books.json` in `C:\Folder` directory:
+```commandline
+python parse_tululu_category.py --start_page 5 --end_page 10 --dest_folder C:\Folder 
+```
+
+Download the books covers images from page 1 to page 10 to `C:\Folder` directory, 
+and create `books.json` file in `C:\jsons` directory:
+```commandline
+python parse_tululu_category.py --end_page 10 --skip_txt --dest_folder C:\Folder --json_path C:\jsons 
+```
+
+Create `books.json` file with books data from all the collection pages:
+```commandline
+python parse_tululu_category.py --skip_img --skip_txt
+```
---
+++ parse_tululu_category.py
@@ -0,0 +1,177 @@
+import os
+import json
+import argparse
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
+TULULU_URL = 'http://tululu.org/'
+IMAGES_FOLDER = 'images/'
+BOOKS_FOLDER = 'books/'
+DESCRIPTION = 'This program parses a collection of books of a certain genre from Tululu online library. '\
+                  'You can download books in txt format and books covers images. '\
+                  'The program generates a json format file with downloaded books data such as: '\
+                  'book title, author, book .txt file path, book cover path, genres and comments. '\
+                  'The collection on Tululu includes a certain number of pages, which you can choose to be '\
+                  'the first (start_page) and the last (end_page) pages to download the books from.'
+
+
+def dir_path(path):
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(f"{path} is not a valid path")
+    if os.access(path, os.W_OK):
+        return path
+    else:
+        raise Exception(f":{path} is not writeable")
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('--start_page', type=int, nargs='?', default=1,
+                        help='The first page to parse.'
+                             'If start_page is not set as an argument by user, it will be set as page number 1.'
+                        )
+    parser.add_argument('--end_page', type=int,
+                        help='The last page to parse.'
+                             'If end_page is not set as an argument by user,'
+                             'it will be set as the last page of the collection.'
+                        )
+    parser.add_argument('--skip_txt', action='store_const', const=True,
+                        help='Do not download book .txt files. Download, if not set as an argument by user.'
+                        )
+    parser.add_argument('--skip_img', action='store_const', const=True,
+                        help='Do not download books covers. Download, if not set as an argument by user.'
+                        )
+    parser.add_argument('--dest_folder', type=dir_path, help='Set books and covers download path.')
+    parser.add_argument('--json_path', type=dir_path, help='Set books.json file path')
+    return parser.parse_args()
+
+
+def get_last_page_num(category_url):
+    response = requests.get(category_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    last_page_num = soup.select('.npage')[-1].text
+    return int(last_page_num)
+
+
+def fetch_books_urls(category_url, start_page, end_page):
+    books_urls = []
+    for page_num in range(start_page, end_page + 1):
+        books_list_url = urljoin(category_url, str(page_num))
+        response = requests.get(books_list_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_from_page_urls = list(
+            urljoin(
+                TULULU_URL,
+                tag.find('a')['href']
+                )
+            for tag in soup.find_all('table', class_='d_book')
+        )
+        books_urls += books_from_page_urls
+    return books_urls
+
+
+def get_book(book_page_url):
+    response = requests.get(book_page_url)
+    response.raise_for_status()
+    if response.url != TULULU_URL:
+        soup = BeautifulSoup(response.text, 'lxml')
+        book_href = soup.select('.d_book tr a')[-3]['href']
+        if 'txt.php' not in book_href:
+            return None
+        book_href = urljoin(
+            TULULU_URL,
+            book_href
+        )
+        book_name = soup.find('h1').text
+        title = book_name.split('\xa0')[0].strip()
+        author = book_name.split('\xa0')[2].strip()
+        image_src = urljoin(
+            TULULU_URL,
+            soup.select_one('.bookimage img')['src']
+        )
+        genres = list(
+            genres_tag.text
+            for genres_tag in soup.select('span.d_book a')
+        )
+        comments = list(
+            comments_tag.select_one('.black').text
+            for comments_tag in soup.select('.texts')
+        )
+        book = {
+            'title': title,
+            'author': author,
+            'comments': comments,
+            'genres': genres,
+            'image_src': image_src,
+            'book_path': book_href,
+        }
+        return book
+
+
+def download_txt(url, filename, folder=BOOKS_FOLDER):
+    response = requests.get(url)
+    response.raise_for_status()
+    if response.url != TULULU_URL:
+        filename += '.txt'
+        file_path = os.path.join(folder, sanitize_filename(filename))
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        return file_path
+
+
+def download_image(url, folder=IMAGES_FOLDER):
+    response = requests.get(url)
+    response.raise_for_status()
+    if response.url != TULULU_URL:
+        filename = url.split('/')[-1]
+        file_path = os.path.join(folder, filename)
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        return file_path
+
+
+def main():
+    science_fiction_collection_url = f"{TULULU_URL}l55/"
+    args = parse_arguments()
+    start_page = args.start_page
+    end_page = args.end_page
+    if end_page is None:
+        end_page = get_last_page_num(science_fiction_collection_url)
+
+    if args.dest_folder is not None:
+        os.chdir(args.dest_folder)
+    os.makedirs(IMAGES_FOLDER, exist_ok=True)
+    os.makedirs(BOOKS_FOLDER, exist_ok=True)
+
+    books = []
+    books_urls = fetch_books_urls(science_fiction_collection_url, start_page, end_page)
+    for book_url in books_urls:
+        book = get_book(book_url)
+        if book is None:
+            continue
+        book_title = book['title']
+        image_url = book['image_src']
+        book_download_url = book['book_path']
+        if not args.skip_img:
+            book['image_src'] = download_image(image_url, folder=IMAGES_FOLDER)
+        else:
+            del book['image_src']
+        if not args.skip_txt:
+            book['book_path'] = download_txt(book_download_url, book_title, folder=BOOKS_FOLDER)
+        else:
+            del book['book_path']
+        books.append(book)
+
+    if args.json_path is not None:
+        os.chdir(args.json_path)
+    with open('books.json', 'w', encoding='utf8') as downloaded_books:
+        json.dump(books, downloaded_books, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
