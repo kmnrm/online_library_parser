31cd8e8019bc499d4aca9b3dc3db6c44db95aa13
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-05-24 00:36:40 +0800

    Provide unique names for downloaded files

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 import os
+import time
 import json
 import argparse
 import requests
@@ -126,7 +127,8 @@ def download_txt(url, filename, folder=BOOKS_FOLDER):
     check_response_status(response)
     if response.url == TULULU_URL:
         return
-    filename += '.txt'
+    timestamp = int(time.time())
+    filename = '_'.join([filename, str(timestamp), '.txt'])
     file_path = os.path.join(folder, sanitize_filename(filename))
     with open(file_path, 'wb') as file:
         file.write(response.content)
@@ -138,7 +140,11 @@ def download_image(url, folder=IMAGES_FOLDER):
     check_response_status(response)
     if response.url == TULULU_URL:
         return
-    filename = url.split('/')[-1]
+    filename = url.split('/')[-1].split('.')
+    timestamp = int(time.time())
+    filename_without_format = '_'.join([filename[0], str(timestamp)])
+    filename_format = filename[-1]
+    filename = '.'.join([filename_without_format, filename_format])
     file_path = os.path.join(folder, filename)
     with open(file_path, 'wb') as file:
         file.write(response.content)
